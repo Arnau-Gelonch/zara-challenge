226feb1c97627d6f9daaafbdbb66020a0969dbdf
Arnau-Gelonch <arnaugelonch@gmail.com>
2025-11-11 21:00:49 +0100

    fix: convert HTTP image URLs to HTTPS to prevent mixed content warnings

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,3 +13,33 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+axiosInstance.interceptors.response.use(
+  response => {
+    const convertHttpToHttps = <T>(obj: T): T => {
+      if (typeof obj === 'string' && obj.startsWith('http://')) {
+        return obj.replace('http://', 'https://') as T;
+      }
+      if (Array.isArray(obj)) {
+        return obj.map(convertHttpToHttps) as T;
+      }
+      if (obj && typeof obj === 'object') {
+        const newObj: Record<string, unknown> = {};
+        for (const key in obj) {
+          newObj[key] = convertHttpToHttps(obj[key]);
+        }
+        return newObj as T;
+      }
+      return obj;
+    };
+
+    if (response.data) {
+      response.data = convertHttpToHttps(response.data);
+    }
+
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
